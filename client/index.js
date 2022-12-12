6fb93a5a94eb6394115be0e146de8b3c0296c94a
Kara Brightwell <kara@153.io>
2022-12-12 11:29:29 +0000

    basic server sent events setup

--- client/index.js
+++ client/index.js
@@ -1,10 +1,33 @@
 import { h, render } from 'https://cdn.skypack.dev/preact'
+import { useState, useEffect } from 'https://cdn.skypack.dev/preact/hooks'
 import htm from 'https://cdn.skypack.dev/htm'
 
 const html = htm.bind(h)
 
-const App = () => html`
-	<h1>it works</h1>
-`
+const source = new EventSource('/events')
+
+function App () {
+	const [messages, setMessages] = useState([])
+
+	useEffect(() => {
+		function listen(event) {
+			setMessages(messages => [...messages, JSON.parse(event.data)].slice(-5))
+		}
+
+		source.addEventListener('message', listen)
+
+		return () => {
+			source.removeEventListener('message', listen)
+		}
+	}, [])
+
+	return html`
+		<ul>
+			${messages.map(date => html`
+				<li>${date}</li>
+			`)}
+		</ul>
+	`
+}
 
 render(html`<${App} />`, document.querySelector('main'))
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const SSE = require('express-sse')
+
+const sse = new SSE([Date.now()])
 
 const app = express()
 
 app.use(express.static('client'))
 app.use('/common', express.static('common'))
+
+app.get('/events', (req, res, next) => { res.flush = () => {}; next() }, sse.init)
+
+setInterval(() => {
+	sse.send(Date.now())
+}, 1000)
 
 app.listen(3030, () => console.log(`listening on http://localhost:3030`))
